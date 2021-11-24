bef3bc1cd7d464f8958982cea1f2cdb03b42556d
ChristinaXinny <ChristinaXinny@gmail.com>
2021-11-24 11:49:49 +0800

    lxy_init
    i

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
-    <v-main>
-      
-    </v-main>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <router-view/>
+  </div>
 </template>
 
 <script>
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  data: () => ({
-    
-  }),
-};
+  name: 'app',
+  components: {
+    // HelloWorld
+  }
+}
 </script>
 
 <style>
-  /* 重设样式，把一般很烦人的样式给去除，✌ */
-  @import "./assets/css/reset.css";
+#app {
+  
+}
 </style>
---
+++ src/views/server/ServerMain.vue
@@ -0,0 +1,123 @@
+<template>
+  <v-app id="inspire">
+    
+
+    <v-navigation-drawer v-model="drawer" app id="my_drawer">
+      <v-sheet color="grey lighten-4" class="pa-4" >
+        <p style="margin-top: 30px; font-size:250%; font-weight: bold;">管理员</p>
+       
+
+        <div>john@vuetifyjs.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item v-for="[icon, text] in links" :key="icon" link>
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+
+    <v-main>
+       <div class="card_wrap">
+                    <v-card class="r_card">
+                        <v-card-title>Ve-histogram</v-card-title>
+                        <v-divider></v-divider>
+                        <div class="item_content">
+                            <div class="chart_wrap">
+                                <ve-histogram
+                                    :data="chartData"
+                                    :extend="chartExtend"
+                                    height="240px"
+                                ></ve-histogram>
+                            </div>
+                        </div>
+                    </v-card>
+                    <v-card class="r_card">
+                        <v-card-title>Ve-line</v-card-title>
+                        <v-divider></v-divider>
+                        <div class="item_content">
+                            <div class="tool">
+                                <div class="t">
+                                    <div class="i">Daily Income</div>
+                                    <div class="r">
+                                        <v-icon>mdi-arrow-up</v-icon>38%
+                                    </div>
+                                </div>
+                                <div class="v">584</div>
+                            </div>
+                            <div class="chart_wrap chart_wrap1">
+                                <ve-line
+                                    :data="chartData2"
+                                    :extend="chartExtend2"
+                                    :settings="chartSettings"
+                                    height="240px"
+                                ></ve-line>
+                            </div>
+                        </div>
+                    </v-card>
+                </div>
+      <Title/>
+      <Table />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Table from "../../components/server/Table.vue";
+import Title from "../../components/server/Title.vue";
+
+export default {
+  components: { 
+    Table,
+    Title 
+  },
+  data: () => ({
+    cards: ["Today", "Yesterday"],
+    // drawer: null,
+    links: [
+      ["mdi-inbox-arrow-down", "Inbox"],
+      ["mdi-send", "Send"],
+      ["mdi-delete", "Trash"],
+      ["mdi-alert-octagon", "Spam"],
+    ],
+    drawer: true,
+    items: [
+      { title: "Home", icon: "mdi-home-city" },
+      { title: "My Account", icon: "mdi-account" },
+      { title: "Users", icon: "mdi-account-group-outline" },
+    ],
+    mini: true,
+  }),
+  methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+};
+</script>
+
+
+<style scoped>
+#my_drawer {
+  
+  box-shadow: 2px 0 6px rgb(0 21 41 / 35%);
+  color: aliceblue;
+}
+</style>
